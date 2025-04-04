522625ebc4aee0e8f29a7c3188006b2fad016e12
Pogoretskiy777 <pogorejx@dukes.jmu.edu>
2025-04-04 13:01:12 -0400

    Added skeleton structure of the main RimpleX class to be ran at launch

---
+++ src/rimplex/RimpleX.java
@@ -0,0 +1,30 @@
+package rimplex;
+
+import java.lang.reflect.InvocationTargetException;
+
+import javax.swing.SwingUtilities;
+
+/**
+ * The main class for the RimpleX application. This is where the process begins.
+ * 
+ * General structure taken from Dr. Bernstein's Serialization Lab:
+ * (https://w3.cs.jmu.edu/bernstdh/web/common/labs/experience_serialization/tempz/index.php) 
+ * 
+ * This work complies with JMU Honor Code.
+ */
+public class RimpleX implements Runnable
+{
+  public static void main(String[] args) throws InterruptedException, InvocationTargetException
+  {
+    // Perform all of the setup activities in the event dispatch thread
+    SwingUtilities.invokeAndWait(new RimpleX());
+  }
+
+  @Override
+  public void run()
+  {
+    // TODO Auto-generated method stub
+    
+  }
+
+}
