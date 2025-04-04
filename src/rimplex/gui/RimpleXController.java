e96f317bf8aa3849f455701f1cc0d707b77e7ab6
Pogoretskiy777 <pogorejx@dukes.jmu.edu>
2025-04-04 13:11:19 -0400

    Added skeleton structure of the application window and controller.

---
+++ src/rimplex/gui/RimpleXController.java
@@ -0,0 +1,59 @@
+package rimplex.gui;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+
+/**
+ * The observer of all GUI components of the RimpleX application.
+ * 
+ * General structure taken from Dr. Bernstein's Serialization Lab:
+ * (https://w3.cs.jmu.edu/bernstdh/web/common/labs/experience_serialization/tempz/index.php)
+ * 
+ * This work complies with JMU Honor Code.
+ */
+public class RimpleXController implements ActionListener, DocumentListener
+{
+  public RimpleXController()
+  {
+    super();
+  }
+
+  @Override
+  /**
+   * Action handler when buttons or clicked.
+   * 
+   * @param ae The ActionEvent that generated the message.
+   */
+  public void actionPerformed(ActionEvent ae)
+  {
+    String ac = ae.getActionCommand();
+    
+    // General structure of handling actions:
+    // if (ac.equals(NAME_OF_BUTTON)) { do stuff...}.
+  }
+
+  @Override
+  public void changedUpdate(DocumentEvent e)
+  {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void insertUpdate(DocumentEvent e)
+  {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void removeUpdate(DocumentEvent e)
+  {
+    // TODO Auto-generated method stub
+    
+  }
+
+}
---
+++ src/rimplex/gui/RimpleXWindow.java
@@ -0,0 +1,28 @@
+package rimplex.gui;
+
+import javax.swing.JFrame;
+
+/**
+ * The main window for the RimpleX application.
+ * 
+ * General structure taken from Dr. Bernstein's Serialization Lab:
+ * (https://w3.cs.jmu.edu/bernstdh/web/common/labs/experience_serialization/tempz/index.php) 
+ * 
+ * This work complies with JMU Honor Code.
+ */
+public class RimpleXWindow extends JFrame
+{
+  private static final long serialVersionUID = 1L;
+  
+  /**
+   * Explicit window constructor.
+   * 
+   * @param controller The observer for all GUI components.
+   */
+  public RimpleXWindow(RimpleXController controller)
+  {
+    super();
+    // TODO: Add Java GUI Components to the main window here
+    // TODO: Set size, layout, all those goodies. Helper functions can be utilized.
+  }
+}
