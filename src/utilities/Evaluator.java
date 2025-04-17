8b97d332f80989a80a617604acb4bdede0778e52
Pogoretskiy777 <pogorejx@dukes.jmu.edu>
2025-04-17 16:18:27 -0400

    Added Evaluator class skeleton

---
+++ src/utilities/Evaluator.java
@@ -0,0 +1,14 @@
+package utilities;
+
+public final class Evaluator
+{
+  private Evaluator() {};
+  
+  
+  public static String evaluate(final String leftOperand, final String operator, final String rightOperand)
+  {
+    // TODO;
+    
+    return "";
+  }
+} 
