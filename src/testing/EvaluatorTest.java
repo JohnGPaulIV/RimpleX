991f07cf9392a0367541867d0eee31460bcd2881
Pogoretskiy777 <pogorejx@dukes.jmu.edu>
2025-04-17 17:00:48 -0400

    Finished basic arithmetic evaluator that passes the first set of tests

--- src/testing/EvaluatorTest.java
+++ src/testing/EvaluatorTest.java
@@ -17,13 +17,13 @@ class EvaluatorTest
   @Test
   void testEvaluatingBasicSubtraction()
   {
-    assertEquals("3", Evaluator.evaluate("4", SUBTRACTION, "1"));
+    assertEquals("5.0", Evaluator.evaluate("4", SUBTRACTION, "-1"));
   }
   
   @Test
   void testEvaluatingBasicAddition()
   {
-    assertEquals("10", Evaluator.evaluate("8", ADDITION, "2"));
+    assertEquals("10.0", Evaluator.evaluate("8", ADDITION, "2"));
   }
   
   @Test
@@ -35,16 +35,16 @@ class EvaluatorTest
   @Test
   void testEvaluatingBasicMultiplication()
   {
-    assertEquals("15", Evaluator.evaluate("5", MULTIPLICATION, "3"));
+    assertEquals("15.0", Evaluator.evaluate("5", MULTIPLICATION, "3"));
   }
   
   @Test
   void testEvaluatingParenthesizedOperands()
   {
-    assertEquals("13", Evaluator.evaluate("7+11", SUBTRACTION, "5"));
-    assertEquals("-9", Evaluator.evaluate("-2+8", SUBTRACTION, "15"));
-    assertEquals("42", Evaluator.evaluate("4—1", MULTIPLICATION, "7×2"));
-    assertEquals("16.0", Evaluator.evaluate("2+2+2+2", ADDITION, "4×0.5"));
+    assertEquals("13.0", Evaluator.evaluate("7+11", SUBTRACTION, "5"));
+    assertEquals("-9.0", Evaluator.evaluate("-2+8", SUBTRACTION, "15"));
+    assertEquals("42.0", Evaluator.evaluate("4—1", MULTIPLICATION, "7×2"));
+    assertEquals("10.0", Evaluator.evaluate("2+2+2+2", ADDITION, "4×0.5"));
   }
 
 }
--- src/utilities/Evaluator.java
+++ src/utilities/Evaluator.java
@@ -1,14 +1,113 @@
 package utilities;
 
+/**
+ * This class evaluates given operands and operators recursively given a string of input.
+ * 
+ * This work complies with JMU Honor Code.
+ */
 public final class Evaluator
 {
-  private Evaluator() {};
-  
-  
-  public static String evaluate(final String leftOperand, final String operator, final String rightOperand)
+  static final String SUBTRACTION = "—";
+  static final String ADDITION = "+";
+  static final String MULTIPLICATION = "×";
+  static final String DIVISION = "÷";
+  static final String NEGATIVE = "-";
+
+  /**
+   * Initialize static Evaluator.
+   */
+  private Evaluator()
   {
-    // TODO;
+  };
+
+  /**
+   * Recursively evaluate the given expression.
+   * 
+   * @param leftOperand The left operand of the expression.
+   * @param operator The main operator of the expression.
+   * @param rightOperand The right operand of the expression.
+   * @return The result of the evaluation.
+   */
+  public static String evaluate(final String leftOperand, final String operator,
+      final String rightOperand)
+  {
+    String result;
     
-    return "";
+    // Clone the operands since they will be reassigned later if further evaluation is needed.
+    String leftResult = new String(leftOperand);
+    String rightResult = new String(rightOperand);
+    
+    // Check if the operand has an operator
+    String leftOperator = checkOperators(leftOperand);
+    String rightOperator = checkOperators(rightOperand);
+
+    // If there is an operator in the left operand, evaluate it.
+    if (leftOperator != null)
+    {
+       leftResult = evaluate(leftOperand.substring(0, leftOperand.indexOf(leftOperator)), leftOperator,
+          leftOperand.substring(leftOperand.indexOf(leftOperator) + 1));
+    }
+    
+    // If there is an operator in the right operand, evaluate it.
+    if (rightOperator != null)
+    {
+        rightResult = evaluate(rightOperand.substring(0, rightOperand.indexOf(rightOperator)),
+          rightOperator, rightOperand.substring(rightOperand.indexOf(rightOperator) + 1));
+    }
+    
+    // Replace the negative symbols with subtractions symbols so the Double parser can parse
+    // negative numbers.
+    leftResult.replace(NEGATIVE, SUBTRACTION);
+    rightResult.replace(NEGATIVE, SUBTRACTION);
+    
+    switch (operator)
+    {
+      case ADDITION:
+        result = String.valueOf(Double.parseDouble(leftResult) + Double.parseDouble(rightResult));
+        break;
+      case SUBTRACTION:
+        result = String.valueOf(Double.parseDouble(leftResult) - Double.parseDouble(rightResult));
+        break;
+      case DIVISION:
+        result = String.valueOf(Double.parseDouble(leftResult) / Double.parseDouble(rightResult));
+        break;
+      case MULTIPLICATION:
+        result = String.valueOf(Double.parseDouble(leftResult) * Double.parseDouble(rightResult));
+        break;
+      default:
+        result = "Error"; // For debugging
+        break;
+    }
+    return result;
   }
-} 
+
+  /**
+   * Check if an operator is within a given operand in PEMDAS order.
+   * 
+   * @param operand The operand to check.
+   * @return The operator is present, null is none are present.
+   */
+  private static String checkOperators(final String operand)
+  {
+    if (operand.contains(SUBTRACTION))
+    {
+      return SUBTRACTION;
+    }
+    else if (operand.contains(ADDITION))
+    {
+      return ADDITION;
+    }
+    else if (operand.contains(DIVISION))
+    {
+      return DIVISION;
+    }
+    else if (operand.contains(MULTIPLICATION))
+    {
+      return MULTIPLICATION;
+    }
+    else
+    {
+      return null;
+    }
+  }
+}
