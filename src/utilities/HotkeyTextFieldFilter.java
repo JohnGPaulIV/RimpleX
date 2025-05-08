fed0acfa1ce1b87bcbd8e9671cd36c3e1ea7694a
Pogoretskiy777 <pogorejx@dukes.jmu.edu>
2025-05-08 17:13:23 -0400

    Fixed hotkey filter to accomodate loading initial shortcuts

--- src/utilities/HotkeyTextFieldFilter.java
+++ src/utilities/HotkeyTextFieldFilter.java
@@ -7,10 +7,24 @@ import javax.swing.text.PlainDocument;
 public class HotkeyTextFieldFilter extends PlainDocument
 {
   private static final long serialVersionUID = 1L;
+  private static boolean initialized = false;
 
   public HotkeyTextFieldFilter()
   {
     super();
+  }
+
+  public void setInitialText(final String str)
+  {
+    try {
+      initialized = true;
+      remove(0, getLength());
+      super.insertString(0, str, null);
+    } catch (BadLocationException e) {
+      e.printStackTrace();
+    } finally {
+      initialized = false;
+    }
   }
 
   public void insertString(int offset, String str, AttributeSet attr)
@@ -19,7 +33,20 @@ public class HotkeyTextFieldFilter extends PlainDocument
     {
       return;
     }
-    if ((getLength() + str.length()) <= 1)
+    
+    if (initialized) {
+      try
+      {
+        super.insertString(offset, str, attr);
+      }
+      catch (BadLocationException e)
+      {
+        e.printStackTrace();
+      }
+      return;
+    }
+    
+    if ((getLength() + str.length() <= 1))
     {
       try
       {
